3daac65b458f7f93f01a2879f159178feb6b1322
Ricardo Martins <ricardo@scarybox.net>
2008-10-19 17:06:36 +0100

    Renamed a few variables.

--- redir.c
+++ redir.c
@@ -37,34 +37,33 @@ void closepipes(int *pipes, int count)
 }
 
 /* Executes several external commands, with pipelines */
-int join(char **argv, int count)
+int join(char **argv, int n_commands)
 {
     int i,status;
-    char *myArgv[count][100];
+    int tot_pipes = 2*(n_commands-1); // Total pipe ends
+    int pipes[tot_pipes];
+    char *myArgv[n_commands][100];
 
-    int bg = 0;
-
-    for (i = 0; i < count; i++)
+    /* Tokenize given commands */
+    for (i = 0; i < n_commands; i++)
         tokenize(myArgv[i], argv[i], DELIMITERS);
 
-    int total = count;
-    int tot_pipes = 2*(total-1); // Total pipe ends
-    int pipes[tot_pipes];
+    /* Initialize pipes */
     for (i = 0; i < tot_pipes; i += 2)
         pipe(pipes+i);
 
-    for (i = 0; i < total; i++) {
+    for (i = 0; i < n_commands; i++) {
         if (fork() == 0) {
-            if (i == 0)            // Primeiro comando
+            if (i == 0)                 /* First command */
                 dup2(pipes[1], 1);
-            else if (i == total-1) // Último comando
+            else if (i == n_commands-1) /* Last comand */
                 if (i == 1)
                     dup2(pipes[0], 0);
                 else if (i%2 == 0)
                     dup2(pipes[i], 0);
                 else
                     dup2(pipes[i+1], 0);
-            else {                 // Tudo o resto
+            else {                      /* Everything in between */
                 if (i%2 == 1) {
                     dup2(pipes[i-1], 0);
                     dup2(pipes[i+2], 1);
@@ -82,12 +81,11 @@ int join(char **argv, int count)
         }
     }
 
-    // only the parent gets here and waits for children to finish
+    /* Only the parent gets here and waits for children to finish */
     closepipes(pipes, tot_pipes);
 
-    if (!bg)
-        for (i = 0; i < total; i++)
-            wait(&status);
+    for (i = 0; i < n_commands; i++)
+        wait(&status);
 
     return 0;
 }
