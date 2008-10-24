544db0737d7767b8846caf4658aa780273157564
Ricardo Martins <ricardo@scarybox.net>
2008-10-24 23:38:52 +0100

    Removed unnecessary prototypes.

--- redir.h
+++ redir.h
@@ -1,7 +1,3 @@
 /* See LICENSE file for copyright and license details. */
 
-int add_pid(pid_t new_pid);
-void hellspawn(char **cmd);
-int external_exec(char **myArgv, int bg);
-int join(char **argv, int count, int bg);
 int run_command(char *buffer, int bg);
