dc36774d737828ca33f2df467f727b6ab5d32c5e
Ricardo Martins <ricardo@scarybox.net>
2008-10-25 21:07:55 +0100

    Added missing include guards.

--- jobs.h
+++ jobs.h
@@ -1,4 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
+#ifndef JOBS_H
+#define JOBS_H
+
 int external_exec(char **myArgv, int bg);
 int run_command(char *buffer, int bg);
+
+#endif /* JOBS_H */
