da9b12bb9a9b4e1f15ff4db951f03f4ce81b2cae
Ricardo Martins <ricardo@scarybox.net>
2008-12-22 15:56:57 +0000

    prompt: added missing include guards.

--- src/prompt.h
+++ src/prompt.h
@@ -1,8 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
+#ifndef PROMPT_H
+#define PROMPT_H
+
 /* Shell colours */
 #define RED      "\033[1;31m"
 #define CLEAR    "\033[0m"
 
 int setPrompt(char *format);
 char *getPrompt(void);
+
+#endif /* PROMPT_H */
